a3bbde0cc3751c7e8d8bb2a4ab7eba3db2fe7d1f
akash <anishganapathi19@gmail.com>
2023-09-04 22:33:36 +0530

    Changes in color
    + Added color in Hero.jsx
    - Removed existing color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#45FFCA]' />
+          <div className='w-5 h-5 rounded-full bg-[#C63D2F]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#45FFCA]'>Anish</span>
+            Hi, I'm <span className='text-[#C63D2F]'>Anish</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
