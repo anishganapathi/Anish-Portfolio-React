15409f3b76f78a51f84d6db9d1b226c45065eaf1
akash <anishganapathi19@gmail.com>
2023-09-05 21:19:46 +0530

    Color Changes
    + Added new color for Balls in Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -20,7 +20,7 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color='#AEE2FF' 
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
