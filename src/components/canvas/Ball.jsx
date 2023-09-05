fb2f20cb45dddd328ae12f796f0a9ce4d10d7799
akash <anishganapathi19@gmail.com>
2023-09-06 00:01:05 +0530

    Updated past changes

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -20,7 +20,7 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#AEE2FF' 
+          color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
