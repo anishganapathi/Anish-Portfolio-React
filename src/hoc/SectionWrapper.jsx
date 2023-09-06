03b36b673071854f3460acaee807fe56a9730c3e
sam7075938131 <sam7075938131@gmail.com>
2023-09-06 22:20:41 +0530

    Update SectionWrapper.jsx

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { staggerContainer } from '../utils/motion'
 
+// Section
 const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
