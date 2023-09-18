520c4fc7c9dcd5e0864007339a2c962647687595
akash <anishganapathi19@gmail.com>
2023-09-18 19:42:47 +0530

    Changes in spline component
    + Added text in the spline component
    + Added classNames for divs

--- src/components/SplineMain.jsx
+++ src/components/SplineMain.jsx
@@ -3,8 +3,12 @@ import Spline from '@splinetool/react-spline';
 function SplineMain() {
   return (
     <>
-    <div>
+    <div className='spline-scene'>
+      <div>
+      <h1 className='text-center text-4xl font-bold text-white'>Hello, I'm <span className='text-primary'>Anish</span></h1>
+     </div>
      <Spline scene="https://prod.spline.design/ou840zI3jHfLl5rQ/scene.splinecode" />
+     
     </div>
     </>
     
